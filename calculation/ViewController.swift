eedcbdbbc2dc6e880c8098e65a855bb6fe29bf2b
卢佳明 <571836353@qq.com>
2016-06-27 11:40:06 +0800

    第三版本：计算器
    定义一个flag，再定一个中间变量（相比上一个版本在编辑框的数量上比原来少一个）即将运算的数字写入到x编辑框中，将结果显示到第二个编辑框，较上一
    个版本增加了运算符“＝”，在代码中的具体运算代码写入到控件“＝”中，使用switch分支语句实现加减乘除的运算，并将计算得到的结果显示到编辑框y
    中，在对除法运算方式的处理上，需要判断被除数是否为0的if条件语句。

--- calculation/ViewController.swift
+++ calculation/ViewController.swift
@@ -9,7 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+     var flag=0
+     var temp=0.0
+ 
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    
+    @IBAction func add(sender: AnyObject) {
+    
+ 
+        if flag==0{
+        temp=(Double)(x.text!)!
+            x.text=""
+            flag=1
+        }
+        flag=1
+    }
+    
+    @IBAction func jian(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=2
+        }
+        flag=2
+    }
+    
+  
+    @IBAction func cheng(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=3
+        }
+        flag=3
+    }
+ 
+    @IBAction func calculation(sender: AnyObject) {
+        switch flag{
+        case 1:
+            temp=temp+(Double)(x.text!)!
+            x.text="\(temp)"
+            break
+        case 2:
+            temp=temp-(Double)(x.text!)!
+            x.text="\(temp)"
+            break
+        case 3:
+            temp=temp*(Double)(x.text!)!
+            x.text="\(temp)"
+            break
+        case 4:
+            if (Double)(x.text!)==0{
+                let alertView=UIAlertView()
+                alertView.title="noise"
+                alertView.message="0不可以做除数"
+                alertView.addButtonWithTitle("确认")
+                alertView.show()
+            }
+            temp=temp+(Double)(x.text!)!
+            x.text="\(temp)"
+            break
+        default:
+            break
+        }
+    }
+    @IBAction func chufa(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=4
+        }
+        flag=4
+    
+    }
+  
+    @IBAction func one(sender: AnyObject) {
+        x.text=x.text!+"1"
+    }
+    @IBAction func two(sender: AnyObject) {
+        x.text=x.text!+"2"
+    }
+    @IBAction func three(sender: AnyObject) {
+        x.text=x.text!+"3"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
